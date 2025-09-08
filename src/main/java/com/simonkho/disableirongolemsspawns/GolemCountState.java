3d0cc7842ccaf81e9b8d6f6b872a15e388df3899
Simonkho <63922818+Simonkrh@users.noreply.github.com>
2025-09-08 20:41:09 +0200

    v1.0.3

--- src/main/java/com/simonkho/disableirongolemsspawns/GolemCountState.java
+++ src/main/java/com/simonkho/disableirongolemsspawns/GolemCountState.java
@@ -11,16 +11,13 @@ import net.minecraft.datafixer.DataFixTypes;
 public class GolemCountState extends PersistentState {
     private int golemCount;
 
-    // --- Codec for serializing/deserializing this state ---
     public static final Codec<GolemCountState> CODEC = RecordCodecBuilder.create(inst -> inst.group(
             Codec.INT.fieldOf("GolemCount").forGetter(GolemCountState::getGolemCount))
             .apply(inst, GolemCountState::newFromInt));
 
-    // --- PersistentStateType holds id + constructor + codec + fix type ---
     public static final PersistentStateType<GolemCountState> TYPE = new PersistentStateType<>("golem_count",
             GolemCountState::new, CODEC, DataFixTypes.LEVEL);
 
-    // Constructors used by supplier/codec
     private GolemCountState() {
         this.golemCount = 0;
     }
@@ -40,9 +37,8 @@ public class GolemCountState extends PersistentState {
         this.markDirty();
     }
 
-    // Helper to fetch/create from a world
     public static GolemCountState get(ServerWorld world) {
         PersistentStateManager mgr = world.getPersistentStateManager();
-        return mgr.getOrCreate(TYPE); // 1-arg overload on 1.21.6
+        return mgr.getOrCreate(TYPE);
     }
 }
